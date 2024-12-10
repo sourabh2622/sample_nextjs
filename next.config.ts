36cf14187a1a1b7b7281073fbdd45838cba045ea
Sourabh Kumbhar <118811938+sourabh2622@users.noreply.github.com>
2024-12-10 17:48:24 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    domains: ['nextui.org'], // Add the domain here
+  },
 };
 
 export default nextConfig;
